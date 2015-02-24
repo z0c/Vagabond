e9c6dbad6db1a5869d4f8beb913075e2d07e53c0
z0c <nvnivs@gmail.com>
2015-02-24 07:09:36 +0000

    Added imgur service

---
+++ Vagabond/Imgur/ImgurService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Vagabond.Imgur
+{
+    public class ImgurService
+    {
+        public IEnumerable<string> GetAlbumImages(string albumId)
+        {
+            var auth = new ImgurNet.Authentication.ClientAuthentication("somecrap", true);
+            var i = new ImgurNet.Imgur(auth);
+            var a = new ImgurNet.ApiEndpoints.AlbumEndpoint(i);
+            var r = a.GetAllImagesFromAlbumAsync(albumId).Result;
+            return r.Data.Select(image => image.Link);
+        }
+    }
+}
